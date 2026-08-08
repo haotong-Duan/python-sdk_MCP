38f2ddcfb7c50da3139323353c453eac17d45ec3
haotong_Duan <15102633721@163.com>
2026-08-08 12:51:48 +0800

    lesson1

---
+++ learning/lesson01/server.py
@@ -0,0 +1,33 @@
+# uv run mcp dev learning/lesson01/server.py
+from typing_extensions import Any
+from mcp.server import MCPServer
+import sys
+import platform
+import os
+
+mcp = MCPServer("My First MCP Server")
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two numbers."""
+    return a + b
+
+@mcp.tool()
+def get_length(s: str) -> int:
+    """Get the length of a string."""
+    return len(s)
+
+@mcp.resource("greeting://{name}")
+def greeting(name: str) -> str:
+    """Generate a greeting message."""
+    return f"Hello, {name}!"
+
+@mcp.tool()
+def get_system_info() -> dict[str, Any]:
+    """Get system information."""
+    return {
+        "platform": platform.platform(),
+        "python_version": sys.version,
+        "current_working_directory": os.getcwd(),
+    }
+
